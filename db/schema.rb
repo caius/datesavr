4c610a33a4829dfa1228bf4d30cc866385fb306c
Caius Durling <dev@caius.name>
2019-07-19 09:38:58 +0100

    Create event_dates table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_065455) do
+ActiveRecord::Schema.define(version: 2019_07_19_080258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "event_dates", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "date"
+    t.uuid "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_event_dates_on_event_id"
+  end
 
   create_table "events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_07_19_065455) do
     t.index ["manage_token"], name: "index_events_on_manage_token", unique: true
   end
 
+  add_foreign_key "event_dates", "events"
 end
